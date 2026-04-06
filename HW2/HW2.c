63fd2179400a4ac6345c3a916d685c6d3f65a5a0
annalee49 <annalee2026@u.northwestern.edu>
2026-04-06 13:15:37 -0500

    servo

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -27,20 +27,35 @@ int main()
     gpio_set_function(PWMPIN, GPIO_FUNC_PWM); // Set the Pin to be PWM
     uint slice_num = pwm_gpio_to_slice_num(PWMPIN); // Get PWM slice number
     // the clock frequency is 150MHz divided by a float from 1 to 255
-    float div = 10; // must be between 1-255
+    float div = 150.0f; // must be between 1-255, this is a chosen value
+    uint16_t wrap = 19999; // when to rollover, must be less than 65535, set to this because of the 20 ms period
+    
     pwm_set_clkdiv(slice_num, div); // sets the clock speed
-    uint16_t wrap = 1500; // when to rollover, must be less than 65535
     pwm_set_wrap(slice_num, wrap); 
     pwm_set_enabled(slice_num, true); // turn on the PWM
 
-    pwm_set_gpio_level(PWMPIN, 1500/2); // set the duty cycle to 50%
+    uint16_t min_level = 1000; //5% duty cycle, 1 ms
+    uint16_t max_level = 8000; //30% duty cycle, 6 ms
+
+    pwm_set_gpio_level(PWMPIN, 1500/2); // set the duty cycle to 50% (old)
 
     // turn on the adc
     adc_init();
     adc_gpio_init(26); // pin GP26 is pin ADC0
     adc_select_input(0); // sample from ADC0
 
+    uint16_t level = min_level; //start at min
+    int8_t step = 10; //trying this granularity for sweep
+    
     while (true) {
         tight_loop_contents(); // do nothing here, the interrupt does the work
+        pwm_set_gpio_level(PWMPIN, level);
+        level += step;
+
+        if (level >= max_level || level <= min_level) {
+            step = -step;
+        }
+
+        sleep_ms(20); //20 ms between each step
     }
 }
