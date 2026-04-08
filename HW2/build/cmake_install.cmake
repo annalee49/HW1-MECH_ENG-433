17b23fece85e3682df26d33fdbe1ddc4de132505
annalee49 <annalee2026@u.northwestern.edu>
2026-04-07 23:09:35 -0500

    testing shit

--- HW2/build/cmake_install.cmake
+++ HW2/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/annalee/Documents/GitHub/MECH_ENG-433/HW2
+# Install script for directory: /Users/annalee/Documents/GitHub/HW1-MECH_ENG-433/HW2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,24 +29,24 @@ endif()
 
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
+  set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Users/annalee/.pico-sdk/toolchain/14_2_Rel1/bin/arm-none-eabi-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/annalee/Documents/GitHub/MECH_ENG-433/HW2/build/pico-sdk/cmake_install.cmake")
+  include("/Users/annalee/Documents/GitHub/HW1-MECH_ENG-433/HW2/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/annalee/Documents/GitHub/MECH_ENG-433/HW2/build/install_local_manifest.txt"
+  file(WRITE "/Users/annalee/Documents/GitHub/HW1-MECH_ENG-433/HW2/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -62,6 +62,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/annalee/Documents/GitHub/MECH_ENG-433/HW2/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/annalee/Documents/GitHub/HW1-MECH_ENG-433/HW2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- HW2/build/pico-sdk/src/common/pico_bit_ops_headers/cmake_install.cmake
+++ HW2/build/pico-sdk/src/common/pico_bit_ops_headers/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/annalee/Documents/GitHub/pico-sdk/src/common/pico_bit_ops_headers
+# Install script for directory: /Users/annalee/.pico-sdk/sdk/2.2.0/src/common/pico_bit_ops_headers
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,17 +29,17 @@ endif()
 
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
+  set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Users/annalee/.pico-sdk/toolchain/14_2_Rel1/bin/arm-none-eabi-objdump")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/annalee/Documents/GitHub/MECH_ENG-433/HW2/build/pico-sdk/src/common/pico_bit_ops_headers/install_local_manifest.txt"
+  file(WRITE "/Users/annalee/Documents/GitHub/HW1-MECH_ENG-433/HW2/build/pico-sdk/src/common/pico_bit_ops_headers/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
