6010cb78643ccd126acda5258372b3c79bc55565
annalee49 <annalee2026@u.northwestern.edu>
2026-04-16 10:24:55 -0500

    tentatively wrote the function

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -4,13 +4,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_SDA 8 //OLED SDA
+#define I2C_SCL 9 //OLED SCL 
+#define I2C_SDA_IMU 16 //iMU SDA
+#define I2C_SCL_IMU 17 //IMU SCL
 // Heartbeat LED pin (on Raspberry Pi Pico)
 #define HEARTBEAT_PIN 15  //GP15 on the Raspberry Pi Pico
 //MPU6050 PCB sets the 7 bit address of the chip to 0b1101000 (0x68).
@@ -38,6 +39,13 @@
 #define GYRO_ZOUT_H  0x47
 #define GYRO_ZOUT_L  0x48
 #define WHO_AM_I     0x75
+
+//OLED defines
+#define OLED_WIDTH 128 //screen is 128 pixels wide
+#define OLED_HEIGHT 64 //screen is 64 pixels talls
+#define CENTER_X (OLED_WIDTH / 2)
+#define CENTER_Y (OLED_HEIGHT / 2)
+#define MAX_LINE_LENGTH 30
 
 //function to write
 void setPin(unsigned char address, unsigned char reg, unsigned char value) {
@@ -107,6 +115,23 @@ bool read_mpu6050_data(float *accel_x, float *accel_y, float *accel_z,
     return true;
 }
 
+//function to write on the OLED screen 
+void draw_accel_lines(float ax, float ay) {
+    ssd1306_clear();
+
+    //make the acceleration values proportional
+    int16_t x_len = (int16_t)(ax * MAX_LINE_LENGTH); //acceleration in x direction
+    int16_t y_len = (int16_t)(ay * MAX_LINE_LENGTH); //acceleration in y direction
+
+    //draw horizontal line (X acceleration)
+    ssd1306_draw_line(CENTER_X, CENTER_Y, CENTER_X + x_len, CENTER_Y, 1);
+
+    //draw vertical line (Y acceleration)
+    ssd1306_draw_line(CENTER_X, CENTER_Y, CENTER_X, CENTER_Y - y_len, 1);
+
+    ssd1306_show();
+}
+
 int main()
 {
     stdio_init_all();
@@ -121,10 +146,22 @@ int main()
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+
+    gpio_set_function(I2C_SDA_IMU, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_IMU, GPIO_FUNC_I2C);
+
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
+
+    gpio_pull_up(I2C_SDA_IMU);
+    gpio_pull_up(I2C_SCL_IMU);
+
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
+    ssd1306_init();  //initialize OLED
+    ssd1306_clear(); //clear display buffer
+    ssd1306_show();  //update display
+    
     //here is my function
     mpu6050_init();
 
@@ -143,10 +180,10 @@ int main()
         gpio_put(HEARTBEAT_PIN, 0);
         sleep_ms(500);  //wait 500 ms
 
-        
         //need to read from the WHO AM I register and verify that it is 0x68 (maybe 0x98)
         unsigned char who_am_i = readPin(MPU6050_ADDR, WHO_AM_I);
 
+        //check the WHO_AM_I register
         if (who_am_i == 0x68) {
             printf("WHO AM I is correct\n");
         } else {
@@ -154,12 +191,9 @@ int main()
         }
         }
 
+        //read the IMU data and draw lines on the OLED 
         if (read_mpu6050_data(&ax, &ay, &az, &temp, &gx, &gy, &gz)) {
-            printf("Accel: X=%.3fg Y=%.3fg Z=%.3fg\n", ax, ay, az);
-            printf("Temp: %.2f C\n", temp);
-            printf("Gyro: X=%.2f Y=%.2f Z=%.2f dps\n", gx, gy, gz);
-        } else {
-            printf("Failed to read MPU6050 data\n");
+            draw_accel_lines(ax, ay);
         }
         sleep_ms(10);  // 100 Hz reading
 
