398873c1a5a2fe231d50df5e8c0c331bfcfb20e0
annalee49 <annalee2026@u.northwestern.edu>
2026-04-23 16:53:40 -0500

    cleaned it up

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -14,8 +14,9 @@
 #define PIN_CS   17 //chip selection pin
 #define PIN_SCK  18 //"clock"
 #define PIN_SPI_TX 19 //same as MOSI
+//LDAC is low, SHDN is high, Vrefs are both HIGH
 
-
+//initializations for the functions
 void write_DAC(int channel, float v);
 static inline void cs_select(uint cs_pin);
 static inline void cs_deselect(uint cs_pin);
@@ -57,10 +58,6 @@ int main()
     gpio_set_dir(PIN_CS, GPIO_OUT);
     gpio_put(PIN_CS, 1);
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
-    //adc_init();
-    // adc_gpio_init(26);
-    // adc_gpio_init(27); //wire this
-
 
     //triangle
     float tri = 0.0;
@@ -68,29 +65,15 @@ int main()
     int direction = 1;
     //steps
     float t = 0;
-    float dt = 0.005;
+    float dt = 0.01;
 
     
     while (true) {
 
-        // //ramp waveform
-        // for (int i=0; i<1024; i++){
-        //     write_dac(0,i);
-        //     sleep_ms(10);
-        // }
-        // sleep_ms(1000);
-
-        // //triangle waveform
-        // spi_write_tri();
-
-        // //sine waveform
-        // spi_write_sine();
-
         //sine wave on DAC A
-        float voltage = (sin(2*M_PI*2*t)+1)*(3.3/2.0);
+        float voltage = (sin(2*M_PI*2*t)+1)*(3.3/2.0); //have to convert to radians, then add one and rescale
+        float voltage = ((sin(2*M_PI*2*t)+1)/2)*3.3;
         write_DAC(0,voltage);
-        //adc_select_input(0);
-        //uint16_t rawA = adc_read();
 
         //triangle wave on DAC B
         tri += direction * step;
@@ -103,16 +86,9 @@ int main()
             direction = 1;
         }
         write_DAC(1,tri);
-        //adc_select_input(1);
-        //uint16_t rawB = adc_read();
 
-        //float voltA = rawA*3.3 / 4095.00;
-        //float voltB = rawB*3.3 / 4095.00;
         sleep_ms(5);
         t += dt;
-
-        //printf("sine wave voltage %.2f",voltA);
-        //printf("tri wave voltage %.2f",voltB);
     }
 }
 
@@ -124,12 +100,13 @@ void write_DAC(int channel, float v){
     if (v>3.3) {
         v=3.3;
     }
-    uint16_t myV = v/3.3*1023;
-    data[0] = 0b01110000;
-    data[0] = data[0] | ((channel&0b1)<<7);
-    data[0] = data[0] | (myV>>6)&0b00001111;
+    uint16_t myV = v/3.3*1023; //0V to 1023 *Vref when gain setting is 1x
+    //0b1111111111
+    data[0] = 0b01110000; //write to DACa, buffered, 1x gain, active mode
+    data[0] = data[0] | ((channel&0b1)<<7); //write to DACb or DACa
+    data[0] = data[0] | (myV>>6)&0b00001111; //we only want the first 4 bits of this
 
-    data[1] = (myV<<2) & 0xFF;
+    data[1] = (myV<<2) & 0xFF; //garbage bits on the ends (11,12)
 
     cs_select(PIN_CS);
     spi_write_blocking(SPI_PORT, data, 2); // where data is a uint8_t array with length len
