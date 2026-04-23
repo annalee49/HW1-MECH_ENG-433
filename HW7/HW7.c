c8b8e3448f94225e00d64eac1c3922159f5ef236
annalee49 <annalee2026@u.northwestern.edu>
2026-04-23 12:03:04 -0500

    basics, no waveforms yet

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -25,21 +25,33 @@
 #define UART_ID uart1
 #define BAUD_RATE 115200
 
+static inline void cs_select(uint cs_pin);
+static inline void cs_deselect(uint cs_pin);
+
+void write_dac(uint8_t channel, uint16_t value)
+{
+    // limit to 10-bit input
+    value &= 0x03FF;
+
+    // scale to 12-bit for MCP4912
+    uint16_t dac_val = value << 2;
+
+    uint16_t command =
+        (channel << 15) |   // 0 = A, 1 = B
+        (1 << 14) |         // buffered
+        (1 << 13) |         // gain = 1x
+        (1 << 12) |         // active
+        (dac_val & 0x0FFF);
+
+    cs_select(PIN_CS);
+    spi_write16_blocking(SPI_PORT, &command, 1);
+    cs_deselect(PIN_CS);
+}
+
 
 int main()
 {
     stdio_init_all();
-
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(spi_default, 12 * 1000); // the baud, or bits per second, starting at 12kHz
-    gpio_set_function(PIN_SPI_RX, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_SPI_TX, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
@@ -50,8 +62,41 @@ int main()
     gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
+
+    // SPI initialisation.
+    spi_init(spi0, 12 * 1000); // the baud, or bits per second, starting at 12kHz
+    gpio_set_function(PIN_SPI_RX, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SPI_TX, GPIO_FUNC_SPI);
+    
+    // Chip select is active-low, so we'll initialise it to a driven-high state
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1);
+    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+
+    //initialize, dont touch cpol or cpha
+    spi_set_format(SPI_PORT, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+
+    // send one DAC value
+    uint16_t dac_val = 2048;
+
+    //shift 0 to bit 15, 1 to bit 14, 1 to bit 13, 1 to bit 12
+    uint16_t command =
+    (0 << 15) |
+    (1 << 14) |
+    (1 << 13) |
+    (1 << 12) |
+    (dac_val & 0x0FFF);
+
+    cs_select(PIN_CS);
+    spi_write16_blocking(SPI_PORT, &command, 1);
+    cs_deselect(PIN_CS);
+    
     while (true) {
-        printf("Hello, world!\n");
+        for (int i=0; i<1024; i++){
+            write_dac(0,i);
+            sleep_ms(10);
+        }
         sleep_ms(1000);
     }
 }
@@ -72,8 +117,9 @@ static inline void cs_deselect(uint cs_pin) {
 
 
 void write_spi(uint8_t data) {
-    size_t len = sizeof(data);
     cs_select(PIN_CS);
-    spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
+    spi_write_blocking(SPI_PORT, &data, 1);
     cs_deselect(PIN_CS);
 }
+
+;
